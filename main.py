4fd5026ff6d5fe9b8f31ca182588478ac9a756bd
Frenki Muca <frenki@muca.com>
2023-07-03 10:55:45 +0200

    editing report file and user input

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def face_matching(face_distance, face_match=0.6):
         return str(round(value, 2)) + '%'
 
 
-class FaceRecognition:
+class Recognition:
     face_locations = []
     face_encodings = []
     face_names = []
@@ -59,10 +59,10 @@ class FaceRecognition:
         window = tk.Tk()
         window.withdraw()
         teacher = simpledialog.askstring("Teacher", "Teacher, can you please introduce yourself?")
-        activity = simpledialog.askstring("Activity", f'{teacher}, what is the activity for today?')
+        activity = simpledialog.askstring("Activity", f'{teacher.split()[0]}, what is the activity for today?')
         window.destroy()
-        video_capture = cv2.VideoCapture('video.mp4')
-        video_capture.set(cv2.CAP_PROP_FPS, 60)
+        start_time = datetime.now()
+        video_capture = cv2.VideoCapture('ASD.mp4')
 
         if not video_capture.isOpened():
             print('Video source not found...')
@@ -147,10 +147,11 @@ class FaceRecognition:
 
         video_capture.release()
         cv2.destroyAllWindows()
+        end_time = datetime.now()
 
         # save a video record
         output_path = 'emotions_record.mp4'
-        output = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*"mp4v"), 30, size)
+        output = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*"mp4v"), 60, size)
 
         for frame in self.frame_list:
             output.write(frame)
@@ -160,32 +161,46 @@ class FaceRecognition:
         file_path = 'people_emotions.txt'
         emotion_with_max_count = {}
         with open(file_path, 'w') as file:
-            now = datetime.now()
-            file.write(f'Today on {now:%d/%m/%Y} teacher {teacher} taught the children: "{activity}"\n\n')
+
+            line0 = f'Today on {start_time:%d/%m/%Y} teacher {teacher} taught the children: "{activity}".\n' \
+                    f'The session began at {start_time:%H:%M} and ended at {end_time:%H:%M}.\n'
+            file.write(f'{line0}\n')
             for person, person_emotions in self.people_emotions.items():
+                if person == "Unknown":
+                    count_unknown = simpledialog.askinteger("Unknown Counter","How many children are not known by the system?")
+                    if count_unknown == 1:
+                        person = simpledialog.askstring('Unknown Child',"Who is the unknown person?")
+                    else:
+                        person = "Many children"
                 emotions_count = ', '.join([f'{emotion} {count} times' for emotion, count in person_emotions.items()])
-                line = f'{person} was {emotions_count}\n'
+                if person == "Many children":
+                    line = f'{person} were {emotions_count}.'
+                else:
+                    line = f'{person} was {emotions_count}.'
                 if person_emotions:
                     max_emotion_count = max(person_emotions.values())
                     max_emotions = [emotion for emotion, count in person_emotions.items() if count == max_emotion_count]
                     emotion_with_max_count[person] = ', '.join(max_emotions)
-                    if len(max_emotions) == 1:
-                        if emotion_with_max_count[person] in ["Happy","Surprised"]:
-                            line2 = f'{person} was {emotion_with_max_count[person]} most of the time. {person} likes "{activity}".\n'
-                        elif emotion_with_max_count[person] in ["Sad", "Angry", "Disgusted", "Fearful"]:
-                            line2 = f'{person} was {emotion_with_max_count[person]} most of the time. {person} does not like "{activity}".\n'
-                        elif emotion_with_max_count[person] == "Neutral":
-                            line2 = f'{person} was {emotion_with_max_count[person]} most of the time. {person} might like "{activity}".\n'
-                    else:
-                        line2 = f'{person} had a mix of emotions while doing: {activity}\n'
                 else:
-                    line2 = f'{person} did not display any emotions while doing: {activity}\n'
-                file.write(f'{line}{line2}\n')
+                    file.write(f'{person} did not display any emotions while doing: {activity}\n')
+                file.write(f'{line}\n')
+            like_activity = [person for person, emotion in emotion_with_max_count.items() if emotion in ["Happy", "Surprised"]]
+            do_not_like_activity = [person for person, emotion in emotion_with_max_count.items() if emotion in ["Sad", "Fearful", "Angry", "Disgusted"]]
+            might_like_activity = [person for person, emotion in emotion_with_max_count.items() if emotion in ["Neutral"]]
+            many_emotions = [person for person, emotion in emotion_with_max_count.items() if len(emotion.split(',')) > 1]
+            if like_activity:
+                file.write(f'\nThe children who like to do "{activity}": {", ".join(like_activity)}.\n')
+            if do_not_like_activity:
+                file.write(f'\nThe children who do not like to do "{activity}": {", ".join(do_not_like_activity)}.\n')
+            if might_like_activity:
+                file.write(f'\nThe children who might like to do "{activity}": {", ".join(might_like_activity)}.\n')
+            if many_emotions:
+                file.write(f'\nThe children who had many different emotions while doing "{activity}": {", ".join(many_emotions)}.\n')
 
         print(f"Video saved to {output_path}")
         print(f"Emotion information saved to {file_path}")
 
 
 if __name__ == "__main__":
-    fr = FaceRecognition()
-    fr.run_recognition()
+    rec = Recognition()
+    rec.run_recognition()
